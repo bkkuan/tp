8755c3e719d53667c4cdad536b3e07d537774f37
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-04-08 03:03:44 +0800

    Fix test cases for index

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,8 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_MEMBER_DISPLAYED_INDEX = "The member index provided is invalid. "
-            + "List to check indexes.\n";
+    public static final String MESSAGE_INVALID_MEMBER_DISPLAYED_INDEX = "Requires a valid and existing member index. "
+            + "Enter LIST to check indexes of existing members.\n";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
