5e0c693312cf669a67aecfd09e6cf54e62e7c02c
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-04-07 23:44:06 +0800

    Fix checkstyleMain errors

--- src/main/java/seedu/address/logic/parser/TaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TaskCommandParser.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_EMPTY_TASK_DESC;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 
 import java.util.logging.Logger;
 
